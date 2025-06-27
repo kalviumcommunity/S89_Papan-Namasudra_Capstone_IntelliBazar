de3e0b5248d848eb88648cea770ebdfc6939b5b9
Papan Namasudra <papannamasudra@ec2-16-16-10-213.eu-north-1.compute.amazonaws.com>
2025-06-27 14:14:10 +0530

    Update

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,11 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
   server: {
+    host: true,
+    port: 5173,
     proxy: {
       '/api': {
         target: 'http://localhost:8080',
@@ -14,5 +15,10 @@ export default defineConfig({
         secure: false
       }
     }
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false
   }
 })
