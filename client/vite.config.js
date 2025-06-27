cf3110c09587d1407448ee31cbe3ba143aa7467c
Papan Namasudra <papannamasudra@ec2-16-16-10-213.eu-north-1.compute.amazonaws.com>
2025-06-27 14:52:42 +0530

    Update

--- client/vite.config.js
+++ client/vite.config.js
@@ -33,6 +33,19 @@ export default defineConfig({
       }
     }
   },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+        assetFileNames: 'assets/[name].[hash].[ext]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js'
+      }
+    }
+  },
   define: {
     global: 'globalThis'
   }
